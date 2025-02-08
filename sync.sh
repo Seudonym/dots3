3484cd814414ee4f24975a4894e4e74f260b05f8
Seudonym <wk170179@gmail.com>
2025-02-08 12:13:04 +0530

    update config

--- sync.sh
+++ sync.sh
@@ -1,9 +1,9 @@
-cp -r ~/.config/hypr hypr
-cp -r ~/.config/waybar waybar 
-cp -r ~/.config/cava cava
-cp -r ~/.config/ghostty ghostty
-cp -r ~/.config/nvim nvim
-cp -r ~/.config/uwsm uwsm
+cp -r ~/.config/hypr .
+cp -r ~/.config/waybar .
+cp -r ~/.config/cava .
+cp -r ~/.config/ghostty .
+cp -r ~/.config/nvim .
+cp -r ~/.config/uwsm .
 git add .
 git commit -m "update config"
 git push origin main
